14e80e61ba367f093c08b2f79bd310084231943a
purreB <jakob.bergstrom@live.se>
2026-03-08 21:08:53 +0100

    Add api prefix

--- lib/api.ts
+++ lib/api.ts
@@ -3,7 +3,7 @@ export const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 
 export async function fetchHotels() {
-  const res = await fetch(`${API_URL}/hotel`); // Adjust URL as needed
+  const res = await fetch(`${API_URL}/api/hotel`);
   return res.json();
 }
 
